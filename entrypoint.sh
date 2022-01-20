c9e03c244b641db04c87c4fee25e157cf4860af0
Raffael Schreiber <49059499+rschre@users.noreply.github.com>
2022-01-20 09:34:35 +0100

    another try removing the parent folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,9 @@ aws configure set default.region "${INPUT_LAMBDA_REGION}" > /dev/null 2>&1
 echo "OK"
 
 echo "Deploying lambda main code..."
-zip -r lambda.zip .${INPUT_SOURCES_DIR}/* -x \*.git\*
+cd ${INPUT_SOURCES_DIR}
+zip -r lambda.zip ./* -x \*.git\*
+cd ..
 aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://lambda.zip
 echo "OK"
 
